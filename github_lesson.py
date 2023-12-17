8699e025bc4b7cc26b6b8d181ad8b24248420382
atvey124 <matveygghhgg@gmail.com>
2023-12-17 07:42:24 -0800

    third algoritm,arithmetic mean

--- github_lesson.py
+++ github_lesson.py
@@ -1,3 +1,4 @@
+#max_number,max_index_number
 a = [65,76,87,98,65]
 index = 0
 max = 0
@@ -7,11 +8,12 @@ while index < len(a):
         max = a[index]
         index_max_number = index
     index += 1
-print(max)
-print(index_max_number)
+print(f"большее число в массиве: {max}")
+print(f"индекс большего числа в массиве: {index_max_number}")
 
 
 
+#min_number,min_index_number
 a = [65,76,87,98,65,6565,88,-2,-1]
 index = 0
 min = a[0]
@@ -21,5 +23,17 @@ while index < len(a):
         min = a[index]
         index_min_number = index
     index += 1
-print(min)
-print(index_min_number)
+print(f"меньшее число: {min}")
+print(f"индекс меньшегго чиисла: {index_min_number}")
+
+
+
+
+#Среднее арефметическое среди всех чисел массива
+a = [54465,767,87,98,656,87,989,56]
+sum = 0
+index = 0
+while index < len(a):
+    sum = sum + a[index]
+    index += 1
+print(f"среднее арефметическое: {sum/len(a)}")
