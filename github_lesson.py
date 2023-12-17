f0ce353f1c5627f8f05ecefb6f6d691902df3988
atvey124 <matveygghhgg@gmail.com>
2023-12-17 07:34:54 -0800

    second algoritm,min_number,index_min_number

--- github_lesson.py
+++ github_lesson.py
@@ -9,3 +9,17 @@ while index < len(a):
     index += 1
 print(max)
 print(index_max_number)
+
+
+
+a = [65,76,87,98,65,6565,88,-2,-1]
+index = 0
+min = a[0]
+index_min_number = index
+while index < len(a):
+    if min > a[index]:
+        min = a[index]
+        index_min_number = index
+    index += 1
+print(min)
+print(index_min_number)
